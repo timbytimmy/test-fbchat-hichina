b5e213c5e12eb1d3031b457c54f987fc52c36bcb
fengchang <fengchang_fight@163.com>
2023-05-27 12:49:43 +0800

    fix typo

--- hichina-admin-backend/src/main/java/com/hichina/admin/hichinaadminbackend/controller/BlogController.java
+++ hichina-admin-backend/src/main/java/com/hichina/admin/hichinaadminbackend/controller/BlogController.java
@@ -7,7 +7,6 @@ import com.hichina.admin.hichinaadminbackend.model.DTO.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-a
 import java.util.List;
 
 @RestController
