1bbcff535366ba3e463bd5f594287d47a1ddee6a
fengchang <fengchang_fight@163.com>
2023-07-14 23:13:18 +0800

    update regsuccess page

--- hichina-main-front-mobile-first/src/pages/RegSuccessPage.vue
+++ hichina-main-front-mobile-first/src/pages/RegSuccessPage.vue
@@ -1,9 +1,33 @@
 <template>
-  <q-page> regsuccess </q-page>
+  <q-page>
+    <div class="row flex flex-center">
+      <div class="col-12 col-md-8 text-h3 q-pa-xl text-red">
+        Please go to your mailbox to confirm registration...
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
+import { onMounted, getCurrentInstance } from "vue";
+import { useQuasar } from "quasar";
 export default {
   name: "RegSuccessPage",
+  setup() {
+    const instance = getCurrentInstance();
+    const app = instance.appContext.app;
+    const gp = app.config.globalProperties;
+    const $q = useQuasar();
+
+    const waitAndGo = async () => {
+      setTimeout(() => {
+        gp.$goPage("/");
+      }, 8000);
+    };
+    onMounted(() => {
+      waitAndGo();
+    });
+    return {};
+  },
 };
 </script>
